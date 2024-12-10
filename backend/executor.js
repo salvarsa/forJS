68f8e2505fd1b68309a29047a6b8e1bc1d7d7fd8
salvarsa <salvarsa.dev13@gmail.com>
2024-12-09 22:51:12 -0500

    .

--- backend/executor.js
+++ backend/executor.js
@@ -1,59 +1,131 @@
-const { workerData, parentPort } = require('worker_threads');
-const ivm = require('isolated-vm');
+const { parentPort } = require("worker_threads");
+const ivm = require("isolated-vm");
+const acorn = require("acorn");
+const walk = require("acorn-walk");
 
-async function executeInIsolate(code) {
-  console.log('Código recibido:', code);
+function extractConsoleLogs(ast, code) {
+  const consoleCalls = [];
 
-  const isolate = new ivm.Isolate({ memoryLimit: 128 }); // Limitar memoria a 128 MB
+  walk.simple(ast, {
+    CallExpression(node) {
+      if (
+        node.callee.type === "MemberExpression" &&
+        node.callee.object.type === "Identifier" &&
+        node.callee.object.name === "console" &&
+        node.callee.property.type === "Identifier" 
+      ) {
+        const consoleType = node.callee.property.name; // Tipo de consola (log, warn, error, etc.)
+        const args = node.arguments.map((arg) => {
+          if (arg.type === "Literal") {
+            return arg.value;
+          } else {
+            return null;
+          }s
+        });
+
+        consoleCalls.push({ type: consoleType, arguments: args });
+      }
+    },
+  });
+
+  return consoleCalls;
+}
+
+async function executeCode(blocks, globalContext = {}) {
+  const isolate = new ivm.Isolate({ memoryLimit: 256 });
   const context = await isolate.createContext();
   const jail = context.global;
+  await jail.set("global", jail.derefInto());
+  await jail.set("globalContext", new ivm.ExternalCopy(globalContext).copyInto());
 
-  await jail.set('global', jail.derefInto());
+  // let consoleOutput = [];
+  // const logFunction = new ivm.Reference((type, ...args) => {
+  //   const processedArgs = args.map(arg => {
+  //     try {
+  //       return JSON.parse(arg);
+  //     } catch {
+  //       return arg;
+  //     }
+  //   });
+  //   console.info('El propio bochin',processedArgs);
+    
+  //   consoleOutput.push({ type, arguments: processedArgs }); // Almacena los argumentos procesados
+  // });
 
-  // Capturar salidas de `console.log`
   let consoleOutput = [];
-  const logFunction = new ivm.Reference((...args) => {
-    consoleOutput.push(args.map(arg => String(arg)).join(' '));
+  const logFunction = new ivm.Reference((type, ...args) => {
+    const message = args.map(arg => String(arg)).join(" ");
+    consoleOutput.push({ type, message }); // Acumula cada mensaje de consola en lugar de sobrescribir
   });
-  await jail.set('log', logFunction);
+  
+  await jail.set("log", logFunction);
 
-  // Inyectar una versión personalizada de `console`
-  const redefineConsole = `
+  const setupConsole = `
     global.console = {
-      log: (...args) => log(...args),
+      log: (...args) => log('log', ...args),
+      error: (...args) => log('error', ...args),
+      warn: (...args) => log('warn', ...args),
+      info: (...args) => log('info', ...args),
+      debug: (...args) => log('debug', ...args),
+      trace: (...args) => log('trace', ...args),
+      group: (...args) => log('group', ...args),
+      groupEnd: () => log('groupEnd'),
+      assert: (condition, ...args) => {
+        if (!condition) log('assert', ...args);
+      }
     };
+    Object.assign(global, globalContext);
   `;
-  const redefineScript = await isolate.compileScript(redefineConsole);
-  await redefineScript.run(context);
+
+  const setupScript = await isolate.compileScript(setupConsole);
+  await setupScript.run(context);
 
   let results = [];
+  for (const { code } of blocks) {
+    const ast = acorn.parse(code, { ecmaVersion: 2020, sourceType: "module" });
+    const logs = extractConsoleLogs(ast, code);
+    consoleOutput.push(...logs);
 
-  // Dividir el código en bloques separados por '\n\n'
-  const blocks = code.split('\n\n').map(block => block.trim()).filter(Boolean);
-
-  for (const block of blocks) {
-    const wrappedCode = `
-      (function() {
-        try {
-          return eval(${JSON.stringify(block)});
-        } catch (error) {
-          return 'Error: ' + error.message;
-        }
-      })()
-    `;
     try {
+      const wrappedCode = `
+        (function() {
+          try {
+            return eval(${JSON.stringify(code)});
+          } catch (error) {
+            return { error: error.message };
+          }
+        })()
+      `;
+      
       const script = await isolate.compileScript(wrappedCode);
       const result = await script.run(context, { timeout: 5000 });
-      results.push(result);
+
+      results.push(result?.error ? `Error: ${result.error}` : result);
+
+      const updatedContext = await jail.get("globalContext");
+      globalContext = new ivm.ExternalCopy(updatedContext).copyInto();
     } catch (error) {
-      results.push('Error: ' + error.message);
+      results.push(`Error: ${error.message}`);
     }
   }
 
   isolate.dispose();
-  return { result: results, consoleOutput };
+
+  consoleOutput = consoleOutput.filter(
+    (log, index, self) =>
+      index === self.findIndex((t) => t.type === log.type && t.message === log.message)
+  );
+
+  const filteredResults = results.filter(result => result !== undefined);
+
+  return { result: filteredResults, consoleOutput };
 }
 
-executeInIsolate(workerData.code)
-  .then(data => parentPort.postMessage(data))
-  .catch(error => parentPort.postMessage({ error: error.message }));
+parentPort.on("message", async ({ blocks, globalContext, requestId, port }) => {
+  try {
+    const result = await executeCode(blocks, globalContext);
+    port.postMessage({ requestId, result });
+  } catch (error) {
+    port.postMessage({ requestId, error: error.message });
+  }
+});
